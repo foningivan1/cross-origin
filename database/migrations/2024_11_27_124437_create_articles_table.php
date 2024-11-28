f6ac5cf158205524405108eb4491347d818ddcf2
ulrich-237 <jordifokou@gmail.com>
2024-11-28 11:22:59 +0100

    Ajout des modèles pour la base de données

--- database/migrations/2024_11_27_124437_create_articles_table.php
+++ database/migrations/2024_11_27_124437_create_articles_table.php
@@ -13,17 +13,22 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->id(); // Identifiant unique pour chaque article
-            $table->string('title'); // Titre de l'article
-            $table->string('author')->nullable(); // Auteur de l'article (nullable si inconnu)
-            $table->date('publication_date'); // Date de publication
-            $table->string('category'); // Catégorie de l'article (exemple : sport, politique)
-            $table->text('content'); // Contenu de l'article
-            $table->string('source'); // Nom de la source (exemple : Le Monde)
-            $table->timestamps(); // Champs created_at et updated_at
-        });
+       Schema::create('articles', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');             // Titre de l'article
+        $table->string('author');            // Auteur de l'article
+        $table->date('published_at');        // Date de publication
+        $table->unsignedBigInteger('category_id'); // Catégorie
+        $table->text('content');             // Contenu de l'article
+        $table->unsignedBigInteger('source_id');   // Source de l'article
+        $table->timestamps();
+
+        // Clés étrangères
+        $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        $table->foreign('source_id')->references('id')->on('sources')->onDelete('cascade');
+       });
     }
+
 
     /**
      * Reverse the migrations.
